b50d0cb15fbc2e68da1aa7eb009ae179acc588ce
dongh99 <ken991019@naver.com>
2022-08-18 20:58:40 +0900

    Feat: tag 기능 구현

--- backend/shop/serializers.py
+++ backend/shop/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Plant
+from .models import Plant, Tag
 
 class PlantSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +11,7 @@ class PlantRegisterSerializer(serializers.ModelSerializer):
         model = Plant
         fields = '__all__'
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('register/', register_plant),
     path('search', search), # search?query=[식물이름]
     path('<str:type>/', get_classified_plants),
-    
+    path('tag/<int:id>', create_tag),
 ]
 
 # <str:type>이 들어 있으면 맨 밑으로 내려야 한다
