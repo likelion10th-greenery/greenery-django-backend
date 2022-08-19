d1207611f7e91a774948c21908474e9ddce6127c
dongh99 <ken991019@naver.com>
2022-08-19 20:18:47 +0900

    Revert "signup"
    This reverts commit ab424065276ee0f114f668062e2b662cb0b38116.

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,11 +2,3 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-
-class CustomUser(AbstractUser):
-    user_id = models.CharField(max_length=50)
-    password1 = models.CharField(max_length=50)
-    password2 = models.CharField(max_length=50)
-    phone_num = models.IntegerField()
-    user_address = models.CharField(max_length=200, blank = True, null = True),
-    user_bool = models.BooleanField(default=False)
