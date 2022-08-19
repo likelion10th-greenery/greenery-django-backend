d1d63e894740d39b1b06d21b045d1e763e988978
dongh99 <ken991019@naver.com>
2022-08-20 01:22:37 +0900

    회원가입 완성

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -12,19 +12,9 @@ from django.contrib.auth.hashers import make_password
 def signup(request):
     serializer = SignupSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save(commit=False)
-        if len(serializer.password1) >= 8 and len(serializer.user_num) == 11:
-            alpha_flag = False
-            num_flag = False
-            for i in serializer.password1:
-                if i.isnum():
-                    num_flag = True
-                elif i.isalpha():
-                    alpha_flag = True
-                if num_flag and alpha_flag:
-                    break
-            if serializer.password1 == serializer.password2:
-                new_user = serializer.save(password = make_password(serializer.validated_data['password1']))
-        auth.login(request, new_user)
-        return Response(status=status.HTTP_200_OK)
+        if len(serializer.validated_data['password']) >= 8 and any(i.isalpha() for i in serializer.validated_data['password']) and any(i.isdigit() for i in serializer.validated_data['password']):   
+            if serializer.validated_data['password'] == serializer.validated_data['password1']:
+                new_user = serializer.save(password = make_password(serializer.validated_data['password']))
+                auth.login(request, new_user)
+                return Response(status=status.HTTP_200_OK)
     return Response(status=status.HTTP_400_BAD_REQUEST)
