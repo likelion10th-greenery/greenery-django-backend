fc72b46ab96f5131366854604a9eafdb36b34211
bosungpark <qkrqhtjd0806@naver.com>
2022-08-19 22:57:34 +0900

    Chore: install rest_framework_jwt

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 app_name = 'accounts'
 
 urlpatterns = [
-    # path('', )
+    path('api-jwt-auth/', obtain_jwt_token),          # JWT 토큰 획득
+    path('api-jwt-auth/refresh/', refresh_jwt_token), # JWT 토큰 갱신
+    path('api-jwt-auth/verify/$', verify_jwt_token),   # JWT 토큰 확인
 ]
