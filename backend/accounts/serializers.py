651d1f7fe195f7af5f529aa9b2af2dab90d8b118
dongh99 <ken991019@naver.com>
2022-09-03 19:26:23 +0900

    feat: modify account model

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(max_length=128)
+
+class SignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username','password','password1','phone_num','user_address','user_bool']
