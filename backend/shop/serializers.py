a96d09b678cc8b54521ecca14a00a8dfba95c4f5
ex-g <ex_g@hufs.ac.kr>
2022-09-25 20:02:24 +0900

    REFACTOR: "shop 리팩토링"
    Tag 로직 수정, Plant class에 img_url 추가, 시간 setting, get_all_plants, get_classified_plants, register_plant 함수 수정, manual.txt 업데이트

--- backend/shop/serializers.py
+++ backend/shop/serializers.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
 from .models import Plant, PlantImage, Tag
 
-class PlantImageRegisterSerializer(serializers.ModelSerializer):
+class PlantImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlantImage
         fields = '__all__'
 
-class PlantSerializer(serializers.ModelSerializer):
+class PlantSerializer(serializers.ModelSerializer): # 구매자가 shop main에서 볼 때 이용
     class Meta:
         model = Plant
         fields = ['id', 'feed_title', 'plant_type', 'price', 'img_url']
 
-class PlantRegisterSerializer(serializers.ModelSerializer): 
+
+class PlantRegisterSerializer(serializers.ModelSerializer): # 판매자가 식물을 register할 때 이용
     class Meta:
         model = Plant
         fields = '__all__'
@@ -19,4 +20,4 @@ class PlantRegisterSerializer(serializers.ModelSerializer):
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = '__all__'
+        fields = ['tag']
--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,3 +1,4 @@
+from functools import partial
 from django.http import JsonResponse
 from django.db import transaction
 from django.db.models import Q
@@ -7,7 +8,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from .models import Plant, PlantImage, Tag
-from .serializers import PlantSerializer, PlantRegisterSerializer, PlantImageRegisterSerializer, TagSerializer
+from .serializers import PlantSerializer, PlantRegisterSerializer, PlantImageSerializer, TagSerializer
 from .utils.PlantTypeCrawler import PlantTypeCrawler
 
 # Create your views here.
@@ -15,33 +16,20 @@ from .utils.PlantTypeCrawler import PlantTypeCrawler
 @api_view(['GET'])
 def get_all_plants(request):
     '''
-    모든 식물 조회
+    모든 식물 조회 (shop main)
     '''
     plants = Plant.objects.all()
-    data=list()
-    for plant in plants:
-        q=Q()
-        q.add(Q(plant=plant), q.AND)
-        q.add(Q(image_number=1), q.AND)
-        images= PlantImage.objects.filter(q)    
-        data.append([PlantRegisterSerializer(plant),PlantImageRegisterSerializer(images)])
-    return Response(data)
+    plant_serializer = PlantSerializer(plants, many=True)
+    return Response(plant_serializer.data, status=status.HTTP_200_OK)
 
-# @api_view(["GET"])
-# def get_all_plants(request):
-#     '''
-#     모든 식물 조회
-#     '''
-#     plants = Plant.objects.all()
-#     serializer = PlantSerializer(many=True)
-#     serializer.validated_data["img_url"] = 
 
 @api_view(['GET'])
-def get_classified_plants(request, type):
+def get_classified_plants(request, category):
     '''
-    카테고리별로 식물 조회
+    카테고리별 식물 조회
     ''' 
-    plants = Plant.objects.filter(category=type)
+    print(category)
+    plants = Plant.objects.filter(category__iexact=category)
     try:
         serializer = PlantSerializer(plants, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -50,18 +38,17 @@ def get_classified_plants(request, type):
 
 
 @api_view(['GET'])
+@transaction.atomic()
 def get_one_plant(request, id):
     '''
-    한 식물 디테일 페이지 조회 (id값으로)
+    id 값을 통한 식물 디테일 페이지 조회
     '''
     try:
-        with transaction.atomic():
-            plant = Plant.objects.get(id=id)
-            plant.add_view_cnt()
-            plant.save()
-        
-            serializer = PlantRegisterSerializer(plant)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        plant = Plant.objects.get(id=id)
+        plant.add_view_cnt()
+        plant.save()
+        serializer = PlantRegisterSerializer(plant)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     except Plant.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -82,55 +69,54 @@ def search(request):
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST'])
-def register_plant(request):
-    '''
-    식물 등록
-    '''
-    try:
-        with transaction.atomic():
-            plant_serializer = PlantRegisterSerializer(data=request.data)  
-            if plant_serializer.is_valid(raise_exception=True):
-                plant_serializer.save()
-                plant_images = sorted(request.data.get("plant_images"),key=lambda x: x["image_number"])
-                plant = Plant.objects.last()
-                for idx, plant_image in enumerate(plant_images, start=1):
-                    image_serializer=PlantImageRegisterSerializer(data=plant_image, partial=True)
-                    if image_serializer.is_valid(raise_exception=True):
-                        image_serializer.save(plant=plant, image_number=idx)
-                
-                plant_tags= request.data.get("plant_tags")
-                print(plant_tags)
-                
-                # for plant_tag in plant_tags:
-                #     candidate_tag=plant_tag.get("tag").replace(" ","")
-                #     duplicated_tag=find_duplicate_tags(candidate_tag)
-                #     if not duplicated_tag:
-                #         tag_serializer= TagSerializer(data=plant_tag)
-                #         if tag_serializer.is_valid(raise_exception=True):
-                #             tag=tag_serializer.save(tag=candidate_tag)
-                #             plant.add_tag(tag)
-                #             plant.save()
-                #     else:
-                #         plant.add_tag(duplicated_tag)
-                #         plant.save()
-
-                return Response(plant_serializer.data, status=status.HTTP_200_OK)
-    except:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
 def find_duplicate_tags(tag):
     """
     중복태그 검출로직
     """
     try:
-        return Tag.objects.get(tag=tag)
+        return Tag.objects.get(tag = tag)
     except:
         return False
 
 
+@api_view(['POST'])
+@transaction.atomic()
+def register_plant(request):
+    '''
+    식물 등록
+    '''
+    try:
+        plant_serializer = PlantRegisterSerializer(data=request.data) # 식물 기본 정보 관련
+        if plant_serializer.is_valid(raise_exception=True):
+            plant_serializer.save()
+        plant = Plant.objects.last()
+
+        for tag in plant.plant_tags.split(): # 식물 태그 관련
+            duplicated_tag = find_duplicate_tags(tag)
+            if not duplicated_tag:
+                tag_serializer = TagSerializer(data=request.data)
+                if tag_serializer.is_valid(raise_exception=True):
+                    tag_serializer.save(tag = tag)
+
+        if request.data.get('plant_images'): # 식물 이미지 관련
+            plant_images = sorted(request.data.get("plant_images"), key = lambda x: x["image_number"])
+            for idx, img in enumerate(plant_images, start = 1):
+                if idx == 1:
+                    plant_serializer.save(img_url=img['image_url'])
+                image_serializer = PlantImageSerializer(data=img, partial=True)
+                if image_serializer.is_valid(raise_exception=True):
+                    image_serializer.save(plant = plant, image_number = idx)
+                    
+        return Response(plant_serializer.data, status=status.HTTP_200_OK)
+
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
 def update_plant_type(request):
+    """
+    크롤링을 통한 식물명 데이터베이스 생성
+    """
     PlantTypeCrawler().crawler()
-    
     data = {'result': "DB update success"}
     return JsonResponse(data, status=status.HTTP_200_OK)
