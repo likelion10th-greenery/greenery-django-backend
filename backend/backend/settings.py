d99ea89b4b31cf2c0ee2784299e30201cca83902
bosung park <81157873+bosungpark@users.noreply.github.com>
2022-12-31 20:14:32 +0900

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,6 +15,11 @@ from decouple import config
 
 import datetime
 
+ALLOWED_HOSTS = [
+    ".ec2-44-204-50-208.compute-1.amazonaws.com"
+]
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
